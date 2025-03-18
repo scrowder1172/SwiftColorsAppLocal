975fb282d995166978d0f364040a2734874ad396
Scott Crowder <scrowder@gmail.com>
2025-03-18 09:36:20 -0500

    Initial Commit

---
+++ SwiftColorsAppLocal/SwiftColorsAppLocalApp.swift
@@ -0,0 +1,20 @@
+//
+// File: SwiftColorsAppLocalApp.swift
+// Project: SwiftColorsAppLocal
+// 
+// Created by SCOTT CROWDER on 3/18/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct SwiftColorsAppLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
