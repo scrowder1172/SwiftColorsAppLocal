673bcc92c6cedfbd0d394aefc8d1bc042de8e7e7
Scott Crowder <scrowder@gmail.com>
2025-03-18 09:44:23 -0500

    create hex color extension

---
+++ SwiftColorsAppLocal/Extensions/Color+extension.swift
@@ -0,0 +1,25 @@
+//
+// File: Color+extension.swift
+// Project: SwiftColorsAppLocal
+// 
+// Created by SCOTT CROWDER on 3/18/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+extension Color {
+    init(hex: String) {
+        let cleanHex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+            .replacingOccurrences(of: "#", with: "")
+        var rgb: UInt64 = 0
+        
+        Scanner(string: cleanHex).scanHexInt64(&rgb)
+        
+        let redValue = Double((rgb >> 16) & 0xFF) / 255.0
+        let greenValue = Double((rgb >> 8) & 0xFF) / 255.0
+        let blueValue = Double(rgb & 0xFF) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue)
+    }
+}
